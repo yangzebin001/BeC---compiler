250b3bad016a61af3fa1cdf8c2d576d83bf48508
BeckoninGshy <405678856@qq.com>
2020-07-05 07:07:45 -0700

    mod armtest .c to .sy

---
+++ testcase/test.c
@@ -0,0 +1,52 @@
+const int INF = 0x1234;
+int nol[];
+int a;
+int bdlf[123] = {1,{2}};
+int main(){
+    int nol[];
+    int no;
+    const int a[5] = {1};
+    int aa[4][2] = {1,{32,12}};
+    const int b = 0;
+    int k = 12, kk = 13;
+    int c = 0102;
+    int d = 0x123;
+    int e = 0XAB1F;
+    e = 0;
+    if(12 > 13){
+        int f = 123 / 2;
+    }else{
+
+    }
+    if(b > c){
+        int f = 123 * a;
+    }
+    if(b > 12){
+        int f = 123 % (a+b);
+    }
+    if(44 > b){
+        int f = 123;
+        break;
+    }
+    a = -a;
+    // if ((b > 2) != 0){
+        
+    // }
+    b = !b;
+    c = +a;
+    while(a > b) a = b;
+    while(a !=  b) c = d;
+    while(a == c && (a+c) > c || d < (a+1+4/2%3*9)) {c = d; continue;}
+    f(a,b);
+    return 0;
+}
+int f(int a, int b){
+    f(a,b);
+    return 1;
+}
+
+int abc[][a] = {}; 
+
+void f(int a[][4], int b[]){
+}
+const int bb = a;
