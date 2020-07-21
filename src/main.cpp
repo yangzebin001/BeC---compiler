e062694e8aab1f7b571ce04a0b6627f5003629de
BeckoninGshy <405678856@qq.com>
2020-07-20 22:48:17 -0700

    add many_global_var case

--- src/main.cpp
+++ src/main.cpp
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
 		// 	}
 		// }
 
-		string zero = "1";
+		string zero = "2";
 		AddExpression* add = new AddExpression(new MulExpression(new UnaryExp(new PrimaryExpression(zero, NULL, NULL), NULL, NULL, NULL)));
 		RETURNStatement* rs = new RETURNStatement(add);
 		Block* b = new Block(*new vector<Statement*>(1,rs));
